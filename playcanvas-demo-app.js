140ea7aaaf1ed8df51adb097da94547d74e3acd2
Vladimir Mironiuk <vladimironiuk@gmail.com>
2020-07-14 17:29:21 +0300

    Add the camera

--- playcanvas-demo-app.js
+++ playcanvas-demo-app.js
@@ -2,5 +2,14 @@
 var canvas = document.getElementById("application-canvas");
 var app = new pc.Application(canvas);
 app.start();
-app.setCanvasFillMode(pc.FILL_MODE_FILL_WINDOW)
-app.setCanvasResolution(pc.RESOLUTION_AUTO)
+app.setCanvasFillMode(pc.FILL_MODE_FILL_WINDOW);
+app.setCanvasResolution(pc.RESOLUTION_AUTO);
+
+// Create camera
+var camera = new pc.Entity();
+camera.addComponent("camera", {
+    clearColor: new pc.Color(0.8, 0.8, 0.8)
+});
+
+app.root.addChild(camera);
+camera.setPosition(0, 0, 7);
