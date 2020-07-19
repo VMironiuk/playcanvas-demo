3d782c677337e156b9dbfa96eaddbeb5cd7833a4
Vladimir Mironiuk <vladimironiuk@gmail.com>
2020-07-19 17:22:15 +0300

    Add cone and cylinder

--- playcanvas-demo-app.js
+++ playcanvas-demo-app.js
@@ -14,12 +14,6 @@ camera.addComponent("camera", {
 app.root.addChild(camera);
 camera.setPosition(0, 0, 7);
 
-// Create cube
-var box = new pc.Entity();
-box.addComponent("model", {type: "box"});
-app.root.addChild(box);
-box.rotate(10, 15, 0);
-
 // Create light
 var light = new pc.Entity();
 light.addComponent("light");
@@ -27,8 +21,39 @@ app.root.addChild(light);
 light.rotate(45, 0, 0);
 app.scene.ambientLight = new pc.Color(0.2, 0.2, 0.2);
 
+// Create cube
+var box = new pc.Entity();
+box.addComponent("model", {type: "box"});
+app.root.addChild(box);
+box.rotate(10, 15, 0);
+box.translate(-2, 0, 0);
+
 // Create cube's material
 var boxMaterial = new pc.PhongMaterial();
 boxMaterial.diffuse.set(0, 0.58, 0.86);
 boxMaterial.update();
 box.model.model.meshInstances[0].material = boxMaterial;
+
+// Create cylinder
+var cylinder = new pc.Entity();
+cylinder.addComponent("model", {type: "cylinder"});
+app.root.addChild(cylinder);
+cylinder.rotate(15, 0, 0);
+
+// Create cylinder's material
+var cylinderMaterial = new pc.PhongMaterial();
+cylinderMaterial.diffuse.set(1, 0.58, 0);
+cylinderMaterial.update();
+cylinder.model.model.meshInstances[0].material = cylinderMaterial;
+
+// Create cone
+var cone = new pc.Entity();
+cone.addComponent("model", {type: "cone"});
+app.root.addChild(cone);
+cone.translate(2, 0, 0);
+
+// Create cone's material
+var coneMaterial = new pc.PhongMaterial();
+coneMaterial.diffuse.set(0.9, 0.9, 0.9);
+coneMaterial.update();
+cone.model.model.meshInstances[0].material = coneMaterial;
